d05d324642f009d4363d5b198f1fa59697c24790
haiquangtran <hai.quang.tran@gmail.com>
2018-08-21 20:38:32 +1200

    added initial project with .gitignore file

---
+++ SearchCommandLineApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchCommandLineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
