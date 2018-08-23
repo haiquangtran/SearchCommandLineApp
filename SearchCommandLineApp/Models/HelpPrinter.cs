14254904da9e7dc06aed04b3d822d46e3a96f3f5
haiquangtran <hai.quang.tran@gmail.com>
2018-08-23 17:57:36 +1200

    added change to help text

--- SearchCommandLineApp/Models/HelpPrinter.cs
+++ SearchCommandLineApp/Models/HelpPrinter.cs
@@ -11,7 +11,7 @@ namespace SearchCommandLineApp.Models
         public void PrintHelp()
         {
             Console.WriteLine("To use this tool, do the following: ");
-            Console.WriteLine("1. Navigate to the directory of the SearchCommandLineApp.exe. ");
+            Console.WriteLine("1. In your terminal, navigate to the directory of the SearchCommandLineApp.exe. ");
             Console.WriteLine("2. Use this tool by using the following syntax in the terminal: \"./SearchCommandLineApp -search \"search-term\" -dataset <organisations | users | tickets> [organisations | users | tickets] [organisations | users | tickets]");
             Console.WriteLine("\"search-term\" is the value you are looking for within the JSON object i.e. \"John\"");
             Console.WriteLine("<organisations | users | tickets>: Represents that it is required to specify one of these datasets.");
